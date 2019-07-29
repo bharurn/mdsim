a33e6509ed3376495ae0e0eab1b2650f4ce58397
Bharath Raghavan <52472738+bharurn@users.noreply.github.com>
2019-07-29 16:00:35 -0400

    Added bounding box

--- output.h
+++ output.h
@@ -1,6 +1,8 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
+#define BOUNDBOX 0
+
 #include <fstream>
 #include <string>
 
@@ -10,19 +12,45 @@ namespace output
 	real a,b,c;
 	int n;
 
+	void boundBox()
+	{
+			n = global::n_prt+1;
+
+			for(int i=0; i<2; i++)
+			{
+				if(i%2 == 0) a = 0.5*global::input["xlen"];
+				else a = -0.5*global::input["xlen"];
+				for(int j=0; j<2; j++)
+				{
+					if(j%2 == 0) b = 0.5*global::input["ylen"];
+					else b = -0.5*global::input["ylen"];
+					for(int k=0; k<2; k++)
+					{
+						if(k%2 == 0) c = 0.5*global::input["zlen"];
+						else c = -0.5*global::input["zlen"];
+						f << "C " << a <<  " " << b << " " << c << '\n';
+						++n;
+					}
+				}
+			}
+			n = global::n_prt+1;
+	}
+
 	void trajectory()
 	{
 		static int t = 0;
-		f << global::n_prt << "\n\n";
+		if(BOUNDBOX) f << global::n_prt+8 << "\n\n";
+		else f << global::n_prt << "\n\n";
 		for(int i=0;i<global::n_prt;i++)
 			f << "H " << global::prt_list[i].pos.x << " " << global::prt_list[i].pos.y << " " << global::prt_list[i].pos.z << " " 
 			<< global::prt_list[i].vel.x << " " << global::prt_list[i].vel.y << " " << global::prt_list[i].vel.z << '\n';
+		if(BOUNDBOX) boundBox();
 		t++;
 	}
 
 	void log(real t)
 	{
-		d << t << "		" << global::etot << "		" << global::temp << '\n'; 
+		d << t << "		" << global::etot << "		" << global::input["temp"] << '\n'; 
 	}
 	
 	void setDumpFile(std::string str1, std::string str2)
