9d98ce49dc2fef21f3f66e2702a802b83e618261
Bharath Raghavan <52472738+bharurn@users.noreply.github.com>
2019-07-29 15:59:37 -0400

    Corrected max_time error, and other changes

--- mean_r_sq.h
+++ mean_r_sq.h
@@ -11,15 +11,19 @@ namespace mean_r_sq
 		real diff;
 	};
 	
-	int time_length = 2;
-	int max_time = global::T_f/time_length;
+	int time_length;
+	int max_time;
 	
 	std::vector<diff_value_datatype> diff_value;
 	
 	std::ofstream f;
 	
 	void init()
-	{
+	{		
+		std::cout << "Setting up mean_r_sq calculation...\n";
+		time_length = (int)global::input["mean_r_sq_time_length"];
+		max_time = global::T_f/time_length;
+		
 		std::vector<atom>::iterator i;
 		diff_value_datatype d;		
 		
@@ -34,7 +38,7 @@ namespace mean_r_sq
 		}
 		
 		f.open("mean_r_sq_diff.txt");
-		f << "All quantities in LJ units.\nt		D\n"; 
+		f << "All quantities in LJ units.\nt		<r^2>\n";
 	}
 	
 	void calculate(int time)
@@ -49,9 +53,9 @@ namespace mean_r_sq
 			{
 				for(int j=0; j<global::n_prt; j++)	
 				{	
-					disp.x = global::prt_list[j].pos.x + global::prt_list[j].ix*global::xlen;
-					disp.y = global::prt_list[j].pos.y + global::prt_list[j].iy*global::ylen;
-					disp.z = global::prt_list[j].pos.z + global::prt_list[j].iz*global::zlen;
+					disp.x = global::prt_list[j].pos.x + global::prt_list[j].ix*global::input["xlen"];
+					disp.y = global::prt_list[j].pos.y + global::prt_list[j].iy*global::input["ylen"];
+					disp.z = global::prt_list[j].pos.z + global::prt_list[j].iz*global::input["zlen"];
 					
 					dr.x = disp.x - diff_value[i].ori[j].x;
 					dr.y = disp.y - diff_value[i].ori[j].y;
@@ -68,17 +72,19 @@ namespace mean_r_sq
 	
 	void output()
 	{
+		std::cout << "Writing mean_r_sq values...\n";
 		int no;
+		
 		for(int i=0; i<max_time; i++)
 		{
 			no = (int)(global::T_f/((i+1)*time_length));
 			diff_value[i].diff /= (global::n_prt*no);
 		}
 		
-		int val = global::T_f/global::dump_time;
+		int val = 1;
 		
-		for(int i=1; i<max_time; i+=val)
-			f << i*global::dT*time_length << "		" << diff_value[i].diff << '\n';
+		for(int i=1; i<max_time; i+=global::input["mean_r_sq_dump"])
+			f << i*global::input["dT"]*time_length << "		" << diff_value[i].diff << '\n';		
 	}
 }
 
