cae0fd1885563cce914acdedf2888993ab92c4b7
Bharath Raghavan <52472738+bharurn@users.noreply.github.com>
2019-07-29 15:58:12 -0400

    Updated display text

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "simulation.h"
 #include "compute.h"
 #include "output.h"
+#include <iostream>
+#include <conio.h>
 
 int main()
 {
+	std::cout << "*****MDSim -- Molecular Dynamics Simulation by by Bharath Raghavan*****\n";
 	simulation::setup();
 	simulation::run();
-	mean_r_sq::output();
+	if(global::input["mean_r_sq_dump"]) mean_r_sq::output();
+	std::cout << "Simulation Done.";
 }
